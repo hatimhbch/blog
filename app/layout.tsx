233f2d0009a812336e8aaa2c7f08f91966a3cba2
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-07-26 23:57:29 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,35 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/headtail/Navbar/navbar";
 import Footer from "./components/headtail/Footer/footer";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: 'Articles of experts in all language programming and science Understanding Java Generics: A Comprehensive Guide',
-  description: 'Java, Python, Html, Css, Javascript, Typescript, Nodejs, Reactjs, Vuejs, Angular, Next.js, Nestjs, Expressjs, Spring, Spring bot, c#, c, c++, .Net, Django',
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* هنا يتم إضافة Google Analytics */}
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-JYVGDQZH4V`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JYVGDQZH4V');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
