3493bdb027d961cf6fbb15d750d374ddb729f074
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-07-27 04:12:39 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Collection from "./components/collection/collection";
 export default function Home() {
   return (
     <>
+    <Metadata seoTitle={'Articles for Programing and Science'} seoDescription={'Now get in elevenai more feature of programing and science articles listen comment, react and save posts'} />
     <Collection />
     </>
   );
