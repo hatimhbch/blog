ced379ee422fa08645d92b312e63e9f8217932ed
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-07-27 13:19:27 +0100

    add sitemap and edit setting

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://elevenai.co', // ضع هنا رابط موقعك
+    generateRobotsTxt: true, // (اختياري) قم بتوليد ملف robots.txt
+    // المزيد من الخيارات هنا
+  }
+  
