879238a2f5b9501c668441f5bd08538dfd362295
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-07-28 02:14:27 +0100

    Update Metadata.tsx

--- app/(articles)/Metadata.tsx
+++ app/(articles)/Metadata.tsx
@@ -1,18 +1,20 @@
+import Head from 'next/head';
+
 interface Props {
-    seoTitle: string;
-    seoDescription: string;
-  }
-  
-  export default function Metadata({ seoTitle, seoDescription }: Props) {
-    return (
-      <>
-        <title>{seoTitle}</title>
-        <meta name="description" content={seoDescription} />
-        <meta property="og:title" content={seoTitle} />
-        <meta property="og:description" content={seoDescription} />
-        <meta property="og:image" content="@/app/favicon.ico" />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:image" content="@/app/favicon.ico" />
-      </>
-    );
-  }
+  seoTitle: string;
+  seoDescription: string;
+}
+
+export default function Metadata({ seoTitle, seoDescription }: Props) {
+  return (
+    <Head>
+      <title>{seoTitle}</title>
+      <meta name="description" content={seoDescription} />
+      <meta property="og:title" content={seoTitle} />
+      <meta property="og:description" content={seoDescription} />
+      <meta property="og:image" content="/favicon.ico" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:image" content="/favicon.ico" />
+    </Head>
+  );
+}
