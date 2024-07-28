ec3b13f1e1e8f235f978c8ef42990dddb94ee7a5
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-07-28 02:08:25 +0100

    Update Metadata.tsx

--- app/(articles)/Metadata.tsx
+++ app/(articles)/Metadata.tsx
@@ -8,6 +8,11 @@ interface Props {
       <>
         <title>{seoTitle}</title>
         <meta name="description" content={seoDescription} />
+        <meta property="og:title" content={seoTitle} />
+        <meta property="og:description" content={seoDescription} />
+        <meta property="og:image" content="@/app/favicon.ico" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:image" content="@/app/favicon.ico" />
       </>
     );
   }
