bc9bed4d47e2d0c96d6e39ecb8a1861231f798d5
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-08-08 00:48:51 +0100

    j

--- next-sitemap.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://elevenai.co',
-    generateRobotsTxt: true,
-    // optional
-    robotsTxtOptions: {
-      policies: [{ userAgent: '*', allow: '/' }],
-    },
-  };
